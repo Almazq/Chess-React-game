25378fc936413b88555067c1c02a4ffa35367f7e
Almazq <almazbidullaev@gmail.com>
2022-02-18 23:38:53 +0600

    My first game React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState,useEffect,useRef} from 'react';
+import ChessBoard from "chessboardjsx"
+import Chess from "chess.js"
 
 function App() {
+  const [fen,setFen] = useState("start");
+  let game = useRef(null);
+  useEffect(()=>{
+    console.log("SAD")
+    game.current = new Chess();
+    console.log(game)
+  },[]);
+  const onDrop = ({sourceSquare,targetSquare})=>{
+    // debugger;
+    let move = game.current.move({
+      from:sourceSquare,
+      to:targetSquare
+    });
+    if(move === null) return;
+    setFen(game.current.fen)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      {game.current && game.current.game_over() ? <h1 >GAME OVER</h1> :<div></div> }
+      <ChessBoard position={fen} onDrop={onDrop} />
     </div>
   );
 }
